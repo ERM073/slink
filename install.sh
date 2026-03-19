45946dcd25e7f47d72b6319853c6b4ad05a5b8af
ERM073 <ERM073@users.noreply.github.com>
2026-03-19 19:09:51 +0900

    Fix dependency order in install.sh

--- install.sh
+++ install.sh
@@ -2,13 +2,19 @@
 set -e
 echo "INIT SLINK_INSTALL..."
 mkdir -p storage data templates static
+
+# Install basic dependencies first
+echo "[+] FETCHING_CORES..."
+pip3 install flask werkzeug requests --quiet
+
 SECRET=$(openssl rand -hex 4)
 PASS=$(openssl rand -base64 12)
 HAHSED_PASS=$(python3 -c "from werkzeug.security import generate_password_hash; print(generate_password_hash('$PASS'))")
+
 cat <<EOF > data/config.json
 {"admin_username": "admin", "admin_password": "$HAHSED_PASS", "admin_secret": "$SECRET"}
 EOF
-pip install flask werkzeug requests --quiet
+
 echo "===================================="
 echo " SLINK READY"
 echo "------------------------------------"
